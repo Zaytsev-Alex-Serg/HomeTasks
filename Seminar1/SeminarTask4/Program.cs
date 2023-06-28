d1b1c274e893519485739d4634b1d6e968de99e7
Александр Зайцев <zeus89@yandex.ru>
2023-06-28 20:29:32 +0300

    Seminar Task 4 Done

---
+++ Seminar1/SeminarTask4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Ввод данных
+Console.Write("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine());
+
+// Деление по модулю и получение остатка
+int result = a % 10;
+
+// Вывод результата
+Console.Write($"Последняя цифра = {result}");
