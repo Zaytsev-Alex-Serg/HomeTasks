a1eb760393ae97b3fd80e86d451068351bfaf9dc
Александр Зайцев <zeus89@yandex.ru>
2023-06-28 21:15:34 +0300

    Home Task 1-4

---
+++ HomeWrok1/HomeTask1-4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ввод данных
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());  
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());  
+
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());  
+
+if (a > b && a > c){
+    Console.WriteLine($"Самое большое число {a}");
+}
+if (a < c && c > b){
+    Console.WriteLine($"Самое большое число {c}");
+}
+if (a < b && b > c){
+    Console.WriteLine($"Самое большое число {b}");
+}
