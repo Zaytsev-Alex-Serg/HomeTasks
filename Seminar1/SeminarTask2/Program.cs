d7ea45da64f18dac175448e372bd2e3842a84b9b
Александр Зайцев <zeus89@yandex.ru>
2023-06-28 20:08:52 +0300

    Second Task Done

---
+++ Seminar1/SeminarTask2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Ввод данных
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (b * b == a){
+    Console.WriteLine("ДА");
+}
+else{
+    Console.WriteLine("НЕТ");
+}
