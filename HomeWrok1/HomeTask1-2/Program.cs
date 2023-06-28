7dcff3efa5c361bb216ece6030d895bf3aff0527
Александр Зайцев <zeus89@yandex.ru>
2023-06-28 20:53:53 +0300

    Home Task 1-2

---
+++ HomeWrok1/HomeTask1-2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ввод данных
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+// Определение большего числа и вывод данных
+
+if (a > b){
+    Console.Write($"max = {a}");
+}
+if (b > a){
+    Console.Write($"max = {b}");
+}
+if (a == b){
+    Console.Write($"Числа {a} и {b} равны");
+}
