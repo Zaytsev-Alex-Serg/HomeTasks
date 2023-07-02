62665bf0fc5c8e9f18c8f913b1a1cf99eb5b4bb7
Александр Зайцев <zeus89@yandex.ru>
2023-07-02 22:24:04 +0300

    Home Task 3-19

---
+++ Seminar3/HomeWork3/HomeTask3-19/Program.cs
@@ -0,0 +1,75 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и
+// проеверяет, является ли оно палиндромом
+
+int Promt (string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+/*void CheckNum (int num)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        num = num/10;
+    }
+    if (num != 0)
+    {
+        Console.Write("Вы ввели не пятизначное число");
+    }
+
+}*/
+
+int Reverse (int a)
+{
+    int pali = 0;
+    int k = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        k = k * 10;
+        pali = a % 10;
+        k += pali;
+        a = a / 10;       
+    }
+
+    return k;
+}
+
+/*bool CheckPali (int num, int numRev)
+{
+    if (num == numRev)
+    {
+        return true;
+    }
+    if (num != numRev)
+    {
+        return false;
+    }
+}*/
+
+int num = Promt("Введите пятизначное число: ");
+//CheckNum(a);
+int numRev = Reverse(num);
+int numCheck = num;
+for (int i = 0; i < 5; i++)
+    {
+        numCheck = numCheck/10;
+    }
+    if (numCheck != 0)
+    {
+        Console.Write("Вы ввели не пятизначное число");
+    }
+    if (numCheck == 0)
+    {
+        Reverse(num);
+    }
+    if (num == numRev)
+    {
+        Console.Write($"Число {num} палиндром");
+    }
+    if (num != numRev)
+    {
+        Console.Write($"Число {num} НЕ палиндром");
+    }
+
