be5dfbecb037472878e14a3422a42d025586e603
Александр Зайцев <zeus89@yandex.ru>
2023-07-02 09:34:47 +0300

    Seminar3/SeminarTask1

---
+++ Seminar3/SeminarTask1/Program.cs
@@ -0,0 +1,15 @@
+﻿// [Возвращаемый тип метода] [Имя метода - глагол]([Параметры метода])
+// {
+//      Код метода
+// }
+
+int GetSum(int a, int b)
+{
+    int sum = a + b;
+    return sum;
+}
+
+
+//
+int mySum = GetSum(10, 15);
+Console.WriteLine(mySum);
