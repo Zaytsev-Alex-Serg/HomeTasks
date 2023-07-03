67d7d28c02eb10801b77946eace39e0b6fc3633e
Александр Зайцев <zeus89@yandex.ru>
2023-07-03 19:08:03 +0300

    Home Task 3-23 Done

---
+++ Seminar3/HomeWork3/HomeTask3-23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int Promt (string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+void GetSquare (int a)
+{
+    for (int i = 1; i <= a; i++)
+        Console.Write($" {i * i * i}");
+}
+
+// -----------------------------------
+
+int a = Promt("Введите число N: ");
+GetSquare(a);
