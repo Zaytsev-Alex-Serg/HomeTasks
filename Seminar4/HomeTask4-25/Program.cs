e55ce5303cf7a8a1f174a68729ade9f48f5948f8
Александр Зайцев <zeus89@yandex.ru>
2023-07-03 22:27:39 +0300

    HomeTask4-25 and HomeTask2-27 Done

---
+++ Seminar4/HomeTask4-25/Program.cs
@@ -0,0 +1,17 @@
+﻿int Promt (string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int num1 = Promt("Введите число 1: ");
+int num2 = Promt("Введите число 2: ");
+
+int k = num1;
+for (int i = 1; i < num2; i++)
+    {
+        num1 = k * num1;
+    }
+
+Console.WriteLine(num1);
---
+++ Seminar4/HomeTask4-27/Program.cs
@@ -0,0 +1,23 @@
+﻿int Promt (string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int sumNum (int a)
+{
+    int num = 0;
+    int k = 0;
+    while (a > 0)
+    {
+        num = a % 10;
+        k += num;
+        a = a / 10;       
+    }
+    return k;
+}
+
+
+int num = Promt("Введите число: ");
+Console.WriteLine(sumNum(num));
